796a95ddab4de3351b3c406d3e4bb43fe9d701d8
SimonLiu125 <43387024+SimonLiu125@users.noreply.github.com>
2018-10-02 18:11:12 -0400

    Update SpectGen.m

--- SpectGen.m
+++ SpectGen.m
@@ -5,18 +5,22 @@ filename = input('Which File?(do not include extension) ','s');
 M=load(char([filename,'.txt']));
 
 %Inputs
-%DP = input('Please specify desired number of data points ')
-DP = 25000;
+%Spectrum Finess
+DP = input('Please specify desired number of data points ')
+%Where the x axis starts
 Start = input('Please specify the shortest wavelength for the spectrum ');
+%Where the x axis ends
 End = input('Please specify the longest wavelength for the spectrum ');
+%Specify the FWHM of the spectrum
 w = input('Please specify FWHM for the spectrum ');
 
-%Putting data into matrices.
+%Reading data into matrices.
 Abs=M(:,1)';
 Int=M(:,2)';
 
 %Generating tranformation matrix
-Trm=ones(DP,100);
+lines=ndims(Abs);
+Trm=ones(DP,lines);
 x=linspace(Start,End,DP)';
 
 %Transformating data matrices
@@ -33,12 +37,12 @@ y=sum(yT,2);
 
 %Save results matrix
 Results=[x,y];
-save('-ascii', char(['Results',filename,'-UV.txt']), 'Results');
+save('-ascii', char(['Results',filename,'-spec.txt']), 'Results');
 
 %Figure configuraion
 Fig = figure;
 plot(x,y,'k','linewidth',2);
-title(char(['UV-Vis spectrum of ',filename]),"fontsize", 14);
+title(char(['Spectrum of ',filename]),"fontsize", 14);
 xlabel('Wavelength / nm','fontsize',12);
 ylabel('Intensity','fontsize',12);
 box off;
